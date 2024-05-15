ad75617b28a61fa29ff0c27efccfdfa5ff1a2bfc
Arielly09 <162440483+Arielly09@users.noreply.github.com>
2024-05-15 11:48:57 -0300

    Add files via upload

---
+++ for.js
@@ -0,0 +1,10 @@
+const numeros = [100, 200, 300, 500, 600];
+
+
+
+    
+
+//indice++ ou indice +=1 ou indice == indice + 1
+for(let indice = 0; indice < 6;indice += 1){
+    console.log(numeros[indice])
+};
